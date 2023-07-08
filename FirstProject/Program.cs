e23f91c58522f0196cfe59449a828b7c5e8f9941
asawski <gldnoyuncugt@gmail.com>
2023-07-08 13:03:31 +0300

    fixed issue about input

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -1,3 +1,14 @@
-﻿Console.Write("Bir sayı giriniz: ");
+﻿start:
+Console.Write("Bir sayı giriniz: ");
+
 string input = Console.ReadLine();
-Console.WriteLine("Girdiğiniz sayı: " + input);
+int Int;
+bool isInteger = int.TryParse(input, out Int);
+
+if (isInteger) {
+    Console.WriteLine("Girdiğiniz sayı: " + Int);
+} else
+{
+    Console.WriteLine("Girdiğiniz bir sayı değil.");
+    goto start;
+}
