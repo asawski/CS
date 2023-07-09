7677cc6fef633598b97bac506696f42c576db65a
asawski <gldnoyuncugt@gmail.com>
2023-07-09 12:35:35 +0300

    taking number from TryParse via function without converting string to int32

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -1,9 +1,9 @@
 ﻿string inputText = "";
 string inputText2 = "";
 
-bool IsInteger(string input, int number = 0) // we just need this function for bool thing. result is useless in TryParse()
+(bool, int) IsInteger(string input, int number = 0)
 {
-    return int.TryParse(input, out number);
+    return (int.TryParse(input, out number), number); // it returns two value. we can use first or second when we need
 }
 
 void InvalidInputError()
@@ -11,29 +11,29 @@ void InvalidInputError()
     Console.WriteLine("Girdiğiniz bir sayı değil.");
 }
 
-while (!IsInteger(inputText))
+while (!IsInteger(inputText).Item1)
 {
     Console.Write("Bir sayı giriniz: ");
     inputText = Console.ReadLine();
 
-    if (!IsInteger(inputText))
+    if (!IsInteger(inputText).Item1)
     {
         InvalidInputError();
     }
 }
 
-while (!IsInteger(inputText2))
+while (!IsInteger(inputText2).Item1)
 {
     Console.Write("2. bir sayı giriniz: ");
     inputText2 = Console.ReadLine();
 
-    if (!IsInteger(inputText2))
+    if (!IsInteger(inputText2).Item1)
     {
         InvalidInputError();
     }
 }
 
-for (int i = 0; i < Convert.ToInt32(inputText2); i++)
+for (int i = 0; i < IsInteger(inputText2).Item2; i++) // i didnt use a variable for here, but the new function i wrote is suitable for usage from anywhere
 {
     Console.WriteLine("Girdiğiniz sayı: " + inputText);
 }
