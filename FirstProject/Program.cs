5c1420664ee4b648bb5bdaf67402e26f8a24a20c
Hamit Enes Gümüş <hamitenes@hotmail.com>
2023-07-09 12:52:39 +0300

    Main class modifications about TryParse method usage.

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -1,39 +1,53 @@
 ﻿string inputText = "";
 string inputText2 = "";
 
-(bool, int) IsInteger(string input, int number = 0)
-{
-    return (int.TryParse(input, out number), number); // it returns two value. we can use first or second when we need
-}
-
-void InvalidInputError()
-{
-    Console.WriteLine("Girdiğiniz bir sayı değil.");
-}
-
-while (!IsInteger(inputText).Item1)
+int numberForPrint;
+bool isValidDataEntered = false;
+do
 {
     Console.Write("Bir sayı giriniz: ");
     inputText = Console.ReadLine();
 
-    if (!IsInteger(inputText).Item1)
+    if (!IsInteger(inputText, out numberForPrint))
     {
         InvalidInputError();
     }
+    else
+    {
+        isValidDataEntered = true;
+    }
 }
+while (!isValidDataEntered &&!inputText.Equals("EXIT"));
 
-while (!IsInteger(inputText2).Item1)
+int loopCount;
+isValidDataEntered = false;
+do
 {
     Console.Write("2. bir sayı giriniz: ");
     inputText2 = Console.ReadLine();
 
-    if (!IsInteger(inputText2).Item1)
+    if (!IsInteger(inputText2, out loopCount))
     {
         InvalidInputError();
     }
+    else
+    {
+        isValidDataEntered= true;
+    }
+}
+while (!isValidDataEntered && !inputText2.Equals("EXIT"));
+
+for (int i = 0; i < loopCount; i++) // i didnt use a variable for here, but the new function i wrote is suitable for usage from anywhere
+{
+    Console.WriteLine("Girdiğiniz sayı: " + numberForPrint);
 }
 
-for (int i = 0; i < IsInteger(inputText2).Item2; i++) // i didnt use a variable for here, but the new function i wrote is suitable for usage from anywhere
+bool IsInteger(string inputText, out int number)
 {
-    Console.WriteLine("Girdiğiniz sayı: " + inputText);
+    return int.TryParse(inputText, out number); // try to parse entered value, otherwise returns false
+}
+
+void InvalidInputError()
+{
+    Console.WriteLine("Girdiğiniz veri bir sayı değil.");
 }
